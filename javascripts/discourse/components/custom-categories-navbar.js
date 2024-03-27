884d14923be1861009700516cc7b83f132e8fb08
Jarek Radosz <jradosz@gmail.com>
2024-03-27 16:53:22 +0100

    DEV: Update linting

--- javascripts/discourse/components/custom-categories-navbar.js
+++ javascripts/discourse/components/custom-categories-navbar.js
@@ -1,6 +1,6 @@
 import Component from "@glimmer/component";
-import { inject as service } from "@ember/service";
 import { tracked } from "@glimmer/tracking";
+import { service } from "@ember/service";
 
 export default class CustomCategoriesNavbar extends Component {
   @service site;
